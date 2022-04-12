e7c2fc9fa316383ff371ec7fc7d0087b1eec2c59
Norman Teodoro <normanteodoro@hotmail.com>
2022-04-12 12:17:05 -0700

    created party model

---
+++ models/party.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const membersSchema = mongoose.Schema({
+    userId : {
+        type: String,
+        required : true
+    },
+    admin : {
+        type : Boolean,
+        default: false
+    }
+}, {timestamps: true})
+
+const requestsSchema = mongoose.Schema({
+    userId : {
+        type: String,
+        required : true
+    },
+    message : {
+        type : String,
+        maxlength : 160
+    }
+}, {timestamps: true})
+
+const partySchema = mongoose.Schema({
+  gameId: {
+    type: String,
+    required: true
+  },  
+  partyName: {
+      type: String,
+      required : true
+  },
+  authorId : {
+      type: String,
+  },
+  members : [membersSchema],
+  requests : [requestsSchema]  
+}, {timestamps: true})
+
+module.exports = mongoose.model('Party', partySchema)
