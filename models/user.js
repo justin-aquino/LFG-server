5b54b4e5c23e7b5c984e2e2c8d2f4d74961acf24
Jamel Scott Fadel <itsjamelscott@gmail.com>
2022-04-12 12:21:20 -0700

    user schema

--- models/user.js
+++ models/user.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose')
 
+
+const User_game = mongoose.Schema({
+  game_fk: String,   // obect ID of associated game
+  username: String // user input username for the game
+})
+
+const Joined_party = mongoose.Schema({
+  party_fk: String,   // obect ID of associated party
+})
+
 const UserSchema = mongoose.Schema({
-  name: String,
+  username: String,
   email: String,
+  games: [User_game],
+  parties: [Joined_party],
   password: String
 })
 
