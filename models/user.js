f54974025c12b1d1335d3d86b58be9d36a2f6adf
Jon Dimaculangan <jondmclngn@gmail.com>
2022-04-15 14:39:31 -0500

    party controller fix, populating parties added

--- models/user.js
+++ models/user.js
@@ -8,7 +8,9 @@ const User_game = mongoose.Schema({
 })
 
 const Joined_party = mongoose.Schema({
-  party_fk: String,   // obect ID of associated party
+  party_fk: String,   // changed by ajmel
+  partyName: String,  // changed by jamel
+  partyDescription: String   // changed by jamel
 })
 
 const UserSchema = mongoose.Schema({
@@ -16,7 +18,7 @@ const UserSchema = mongoose.Schema({
   email: String,
   games: [User_game],
   parties: [Joined_party],
-  
+
   password: String
 })
 
